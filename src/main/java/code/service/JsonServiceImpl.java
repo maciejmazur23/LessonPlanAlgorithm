d7f91a50106fcf6361ebf7571c634fce27b8062a
Maciej Mazur <maciek.m2317@vp.pl>
2023-01-14 20:15:33 +0100

    Change of loading data from input.json

--- src/main/java/code/service/JsonServiceImpl.java
+++ src/main/java/code/service/JsonServiceImpl.java
@@ -5,13 +5,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class JsonServiceImpl implements JsonService{
+public class JsonServiceImpl implements JsonService {
 
     @Override
     public List<Lesson> getListFromJson(String jsonText) {
@@ -21,16 +22,25 @@ public class JsonServiceImpl implements JsonService{
 
     public String getJsonFromList(List<Lesson> chromosome) {
         return new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create().toJson(chromosome);
+
     }
 
     public String getJsonText() {
-        String string = "";
+        String text = "";
         try {
-            string = Files.readString(Path.of("src/main/resources/input.json"));
-        } catch (IOException e) {
+            Class<?> forName = Class.forName("code.AlgorithmRunner");
+            InputStream stream = forName.getResourceAsStream("/input.json");
+
+            if (stream != null)
+                text = new BufferedReader(
+                        new InputStreamReader(stream, StandardCharsets.UTF_8))
+                        .lines()
+                        .collect(Collectors.joining("\n"));
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return string;
+        return text;
     }
 
     public void saveJsonFile(String json) {
