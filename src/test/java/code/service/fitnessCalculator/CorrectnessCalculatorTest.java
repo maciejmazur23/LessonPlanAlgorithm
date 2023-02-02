17469978cdc0d9a29d2c7c3d0f6db0d8da811419
Maciej Mazur <maciek.m2317@vp.pl>
2023-02-02 14:55:47 +0100

    Add tests to CorrectnessCalculator.java

--- src/test/java/code/service/fitnessCalculator/CorrectnessCalculatorTest.java
+++ src/test/java/code/service/fitnessCalculator/CorrectnessCalculatorTest.java
@@ -21,20 +21,16 @@ class CorrectnessCalculatorTest {
     @BeforeEach
     void setUp() {
         lessons = new ArrayList<>();
-        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
-        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_55_9_40, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
-        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H9_50_10_35, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
-        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H10_55_11_40, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
-        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H11_50_12_35, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
-        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H12_45_13_30, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
-        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H13_40_14_25, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
-        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H14_35_15_20, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
     }
 
     @Test
     @DisplayName("All is right")
     void test1() {
         //given
+        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.A1, TEACHER.SP, SUBJECT.POLISH, ROOM.R_1));
+        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_55_9_40, GROUP.A1, TEACHER.AL, SUBJECT.MATH, ROOM.R_1));
+        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H9_50_10_35, GROUP.A1, TEACHER.JW, SUBJECT.GEOGRAPHY, ROOM.R_1));
+        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H10_55_11_40, GROUP.A1, TEACHER.TJ, SUBJECT.WF, ROOM.R_WF1));
         int expected = 0;
 
         //when
@@ -51,7 +47,7 @@ class CorrectnessCalculatorTest {
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.A1, TEACHER.SP, SUBJECT.POLISH, ROOM.R_2));
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.A1, TEACHER.JW, SUBJECT.GEOGRAPHY, ROOM.R_3));
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.A1, TEACHER.TJ, SUBJECT.WF, ROOM.R_4));
-        int expected = 3;
+        int expected = 2;
 
         //when
         int result = correctnessCalculator.calculateFitness(lessons);
@@ -67,7 +63,7 @@ class CorrectnessCalculatorTest {
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.A2, TEACHER.SP, SUBJECT.POLISH, ROOM.R_1));
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.B3, TEACHER.AS, SUBJECT.ENGLISH, ROOM.R_1));
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.C1, TEACHER.JM, SUBJECT.HIS, ROOM.R_1));
-        int expected = 3;
+        int expected = 2;
 
         //when
         int result = correctnessCalculator.calculateFitness(lessons);
@@ -78,12 +74,29 @@ class CorrectnessCalculatorTest {
 
 
     @Test
-    @DisplayName("Many lessons in that same teacher in one time")
+    @DisplayName("Many lessons with that same teacher in one time")
     void test4(){
         //given
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.C1, TEACHER.AL, SUBJECT.MATH, ROOM.R_2));
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.D2, TEACHER.AL, SUBJECT.MATH, ROOM.R_3));
         lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.E3, TEACHER.AL, SUBJECT.MATH, ROOM.R_4));
+        int expected = 2;
+
+        //when
+        int result = correctnessCalculator.calculateFitness(lessons);
+
+        //then
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Lessons with that same teacher, class and room in one time")
+    void test5(){
+        //given
+        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.C1, TEACHER.AL, SUBJECT.MATH, ROOM.R_2));
+        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.D2, TEACHER.AL, SUBJECT.MATH, ROOM.R_3));
+        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.E3, TEACHER.SP, SUBJECT.POLISH, ROOM.R_3));
+        lessons.add(new Lesson(DAYS.MONDAY, HOUR.H8_00_8_45, GROUP.E3, TEACHER.JW, SUBJECT.GEOGRAPHY, ROOM.R_4));
         int expected = 3;
 
         //when
